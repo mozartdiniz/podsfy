523cf54abf201a155dd766984d628993c01e59ff
Mozart Diniz <mozart.diniz@gmail.com>
2018-02-18 11:18:36 +0100

    Removing firebase config

--- src/conf/firebase.js
+++ src/conf/firebase.js
@@ -1,12 +1,12 @@
 import firebase from 'firebase';
 
 const config = {
-    apiKey: "AIzaSyATYFNsBQcZrdqtIFAXSXJBWqYKuv2xq84",
-    authDomain: "podsfy.firebaseapp.com",
-    databaseURL: "https://podsfy.firebaseio.com",
-    projectId: "podsfy",
-    storageBucket: "podsfy.appspot.com",
-    messagingSenderId: "138675775988"
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
 };
 
 firebase.initializeApp(config);
